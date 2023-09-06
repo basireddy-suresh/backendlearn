88b61c686aca698e45eabb38b818599eea76481b
Basireddy Suresh Kumar Reddy <112562405+basireddy-suresh@users.noreply.github.com>
2023-09-06 12:17:22 +0530

    Add files via upload

---
+++ controller/users.js
@@ -0,0 +1,6 @@
+const data=require('../models/Data.json');
+function getUsers(req,res){
+    const users=data;
+    res.send({users});
+}
+module.exports={getUsers};
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express=require('express');
 const port=8000;
 const app=express();
+const data=require('./models/Data.json');
+const userRouters=require('./router/users');
 // node is runtime env for javascript to run in outside the web browser
 // it can create the server 
 // need to install [npm init] and [npm i express]
@@ -13,6 +15,9 @@ const app=express();
 // git commands
 // git init, git add . , git commit -m "comment"
 // git branch -M main, git remote add origin "link", git push -u origin main
+app.use('/',userRouters);
+// https://github.com/himanshugarg702/backendlearn
+
 app.listen(port,function(err){
     if(err){
         console.log("Error is run in server");
